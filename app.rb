127fec7761ec3778208d288ce6e4312c64eea94f
Megan Comeau <mlcomeau99@gmail.com>
2020-05-27 01:12:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do
-        erb :user_input
-    end
+    erb :user_input
+  end
 
     post '/piglatinize' do
         @piglatin = PigLatinizer.new.piglatinize(params[:user_phrase])
